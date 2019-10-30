16955138dad27fb78a41bf0384575bd3511cbe4a
alex-muliande <amuliande@gmail.com>
2019-10-30 08:21:29 +0300

    add a neighborhood model

--- myhood/models.py
+++ myhood/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+
+
+
+class Neighbourhood(models.Model):
+    Name = models.TextField()
+    display = models.ImageField(upload_to='groups/', default='groups/group.png')
+    description = models.TextField(default='Random group')
+    police = models.TextField(default="999")
+    health = models.TextField(default="213")
+
+    def __str__(self):
+        return self.Name
+
+
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=200,blank=True)
+    contact_info = models.CharField(max_length=200,blank=True)
+    profile_Id = models.IntegerField(default=0)
+    profile_picture = models.ImageField(upload_to='users/', default='user.png')
+    bio = models.TextField(default="Welcome!")
+
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_location(self, location):
+        self. location = location
+        self.save()
+
+    def update_bio(self,bio):
+        self.bio = bio
+        self.save()  
+
+    def __str__(self):
+        return f'{self.user.username} Profile' 
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+
+
