c47f134b0c1f0f41b84187b1e2c0fe37e89b4ba0
alex-muliande <amuliande@gmail.com>
2019-10-31 16:15:10 +0300

    add media changes of images

--- myhood/admin.py
+++ myhood/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,Neighbourhood
+from .models import Profile,Neighbourhood,Businesses,User
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Businesses)
 admin.site.register(Neighbourhood)
--- myhood/models.py
+++ myhood/models.py
@@ -2,25 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-# Create your models here.
-
-
-
-
-class Neighbourhood(models.Model):
-    Name = models.TextField()
-    display = models.ImageField(upload_to='groups/', default='groups/group.png')
-    description = models.TextField(default='Random group')
-    police = models.TextField(default="999")
-    health = models.TextField(default="213")
-
-    def __str__(self):
-        return self.Name
-
-
-
-
-
+# Create your models here
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -53,4 +35,55 @@ class Profile(models.Model):
     def save(self,*args, **kwargs):
         super().save(*args, **kwargs)
 
+class Neighbourhood(models.Model):
+    hood_name = models.CharField(max_length=50 )
+    location = models.CharField(max_length=50 )
+    occupants = models.IntegerField(default=0)
+    admin = models.OneToOneField(User,unique = True , on_delete=models.CASCADE,null=True,default=True)    
+    def create_neighbourhood(self):
+        self.save()
 
+    def delete_neighbourhood(self):
+        self.delete()
+
+    def update_neighbourhood(self):
+        self.save()
+    
+    def update_occupants(self):
+        self.save()
+
+    def __str__(self):
+        return self.location
+
+class Businesses(models.Model):
+    business_name = models.CharField(max_length=100)
+    business_email = models.EmailField()
+    bussiness_description= models.TextField(default=True)
+    neighbourhood=models.ForeignKey(Neighbourhood,on_delete=models.CASCADE, null=True)
+    logo = models.ImageField(upload_to='media/', blank = True, null =True)
+    # user = models.OneToOneField(Profile, default=True,on_delete=models.CASCADE,null=True)
+
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.save()
+
+    def update_business(self):
+        self.save()
+
+    def __str__(self):
+        return self.business_name
+
+
+class Post(models.Model):
+    title=models.CharField(max_length=30)
+    description=models.TextField(max_length=250)
+    address = models.CharField(max_length=30)
+    def __str__(self):
+        return f'{self.title}'
+
+      
+    def save_post(self):
+        self.save()
